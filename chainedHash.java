c82e459b495c986d44d0e28022e87cad3e08d02a
Mi-aadH <4353521@myuwc.ac.za>
2026-03-04 15:10:38 +0200

    Update chainedHash.java

--- chainedHash.java
+++ chainedHash.java
@@ -1,1 +1,55 @@
+import java.util.LinkedList;
+
+public class chainedHash {
+    private static class Entry {
+        String key;
+        String value;
+
+        Entry(String k, String v) {
+            key = k;
+            value = v;
+
+        }
+    }
+
+    private LinkedList<Entry>[] table;
+    private int m;
+
+    public chainedHash(int m){
+        this.m = m;
+        table = new LinkedList[m];
+        for(int i = 0; i<m; i++){
+            table[i] = new LinkedList<>();
+        }
+    }
+        l
+    private int hash(String key) {
+        return Math.abs(key.hashCode()) % m;
+    }
+
+    public void insert (String key, String value) {
+        int index = hash(key);
+
+        LinkedList<Entry> list = table[index];
+        for ( Entry e : list){
+            if ( e.key.equals(key)){
+                e.value = value;
+                return;
+            }
+        }
+        list.add(new Entry (key, value));
+    }
+
+    public String lookup (String key, String value) {
+        int index = hash(key);
+        for (Entry e : table[index]) {
+            if (e.key.equals(key)) {
+                return e.value;
+
+            }
+        }
+        return null;
+    }
+
+}
 
